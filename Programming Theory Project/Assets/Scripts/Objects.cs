179794f928e004224d1d7b4a9ace9151bed7a6d8
AlexBLLL <98815937+AlexBLLL@users.noreply.github.com>
2022-02-11 18:42:05 +0100

    title screen
    -added cool text title
    -added cool start button

---
+++ Programming Theory Project/Assets/Scripts/Objects.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class Objects : MonoBehaviour
+{
+   [SerializeField]
+    private TextMeshProUGUI descriptionText;
+
+    protected int age = 0;
+    new protected string name = "object";
+    protected string description = "object description";
+
+
+    void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+            ShowDescriptionText();
+        }
+
+    }
+
+    public void ShowDescriptionText()
+    {
+        descriptionText.text = $"name: {name}\nage: {age}\n\ndesc: {description}";
+    }
+  
+}
